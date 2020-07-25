d4024f372ade2954c1430e8c1517edbf22d5e1ee
Doug Dyson <dougdyson@gmail.com>
2020-07-25 20:19:54 +0000

    First version

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+/*
+REQUIREMENTS:
+Find and return a value of key of an params
+*/
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function name(params, genre ) {
+ 
+  let matchedKey;
+  let keys = Object.keys(params); 
+  
+  for (const iterator of keys) {
+    if (iterator === genre) {
+      matchedKey = iterator;
+    }
+  }
+
+  // for (let i = 0; i < keys.length; i++) {
+  //   const element = keys[i];
+  //   if (element === genre) {
+  //     matchedKey = element;
+  //   }
+  // }
+  return matchedKey;
+}
+console.log(findKeyByValue(bestTVShowsByGenre, 'sci_fi'));
