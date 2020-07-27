50455c1170b991bb824ae49f4a8fb6b72dc802b6
Doug Dyson <dougdyson@gmail.com>
2020-07-27 04:40:15 +0000

    callbacks

---
+++ map.js
@@ -0,0 +1,16 @@
+/*
+REQUIREMENTS
+Use map to return array if the first character of of every string in
+*/
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
