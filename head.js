fe6447887cd3888c9eb17946ec29b3b1123f44db
Doug Dyson <dougdyson@gmail.com>
2020-07-29 23:50:04 +0000

    Revert "working before refactoring head"
    This reverts commit 378a204234ef2d8720901bdf39889183e19f91d0.

--- head.js
+++ head.js
@@ -1,22 +1,18 @@
-const assertEqual = require('./assertEqual');
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === undefined || expected === undefined) {
-//     return;
-//   } else if (actual === expected) {
-//     return `\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`;
-//   } else {
-//     return `\u274C\u274C\u274C Assertion Failed: ${actual} !== ${expected}`;
-//   }
-// };
+const assertEqual = function(actual, expected) {
+  if (actual === undefined || expected === undefined) {
+    return;
+  } else if (actual === expected) {
+    return `\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `\u274C\u274C\u274C Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
 
 const head = function(array){
   return array[0];
 }
 //TESTS
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([])));
-
-
+//console.log(assertEqual(head([5,6,7]), 5));
+//console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+//console.log(assertEqual(head([])));
 
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -5,11 +5,4 @@ const assertEqual = require('../assertEqual');
 //   // ...
 // }
 
-// if (actual === expected) {
-//   return `\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`;
-// } else {
-//   return `\u274C\u274C\u274C Assertion Failed: ${actual} === ${expected}`;
-// }
-
-
 module.exports = assertEqual;
