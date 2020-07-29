378a204234ef2d8720901bdf39889183e19f91d0
Doug Dyson <dougdyson@gmail.com>
2020-07-29 23:30:04 +0000

    working before refactoring head

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -5,4 +5,11 @@ const assertEqual = require('../assertEqual');
 //   // ...
 // }
 
+// if (actual === expected) {
+//   return `\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`;
+// } else {
+//   return `\u274C\u274C\u274C Assertion Failed: ${actual} === ${expected}`;
+// }
+
+
 module.exports = assertEqual;
