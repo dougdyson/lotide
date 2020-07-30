3f21e0e1f651a925458861602adb36b16dfefd8e
Doug Dyson <dougdyson@gmail.com>
2020-07-30 00:03:41 +0000

    Revert "Revert "Revert "Revert "Revert "starting head refactor"""""
    This reverts commit 7179c26bfbbd18a904cda6136c1dc7b6837333dd.

--- head.js
+++ head.js
@@ -12,7 +12,7 @@ const head = function(array){
   return array[0];
 }
 //TESTS
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([])));
+//console.log(assertEqual(head([5,6,7]), 5));
+//console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+//console.log(assertEqual(head([])));
 
